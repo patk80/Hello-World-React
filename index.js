09378016cfe0c4338cbc3a3805c8b2310dc8125e
Pat Kelly <50188152+patk80@users.noreply.github.com>
2020-08-28 23:42:15 -0400

    Adding function component as a comment
    Adding function component as a comment - tested it out and it worked but learning how to use class components instead at the moment

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ class Hello extends React.Component {
     }
 }
 
+// function Hello() {
+//     return (
+//         <div>
+//             <h1>Hello World!</h1>
+//             <h2>Hello World!</h2>
+//             <h3>Hello World!</h3>
+//             <h4>Hello World!</h4>
+//         </div>
+//     );
+// }
+
 ReactDOM.render(<Hello/>, document.getElementById("root"))
